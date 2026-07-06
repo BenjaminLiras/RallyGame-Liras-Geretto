f328a8f6c49a6dff2ba35bf5459abe20353b707d
Cantale08 <santino.cantale@gmail.com>
2026-07-06 16:33:52 -0300

    SI

--- rally-game/Scripts/area_2d.gd
+++ rally-game/Scripts/area_2d.gd
@@ -1,4 +1,4 @@
-extends Path2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,8 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass
